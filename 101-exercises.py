4e976df7acec48b680f8605caafe2ec26ee06804
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-16 12:53:00 -0500

    start last section, dictionaries

--- 101-exercises.py
+++ 101-exercises.py
@@ -1448,6 +1448,8 @@ print(set([1, 2, 2, 3, 3, 3, 4, 4, 4, 4]))
 
 # Exercise 83
 # Write a function definition named get_unique_values that takes in a list and returns a set with only the unique values from that list.
+def get_unique_values(list):
+    return set(list)
 
 assert get_unique_values(["ant", "ant", "mosquito", "mosquito", "ladybug"]) == {"ant", "mosquito", "ladybug"}
 assert get_unique_values(["b", "a", "n", "a", "n", "a", "s"]) == {"b", "a", "n", "s"}
@@ -1460,6 +1462,8 @@ print("Exercise 83 is correct.")
 
 # Exercise 84
 # Write a function definition named get_unique_values_from_two_lists that takes two lists and returns a single set with only the unique values
+def get_unique_values_from_two_lists(list1, list2):
+    return set(list1 + list2)
 
 assert get_unique_values_from_two_lists([5, 1, 2, 3], [3, 4, 5, 5]) == {1, 2, 3, 4, 5}
 assert get_unique_values_from_two_lists([1, 1], [2, 2, 3]) == {1, 2, 3}
@@ -1472,6 +1476,11 @@ print("Exercise 84 is correct.")
 
 # Exercise 85
 # Write a function definition named get_values_in_common that takes two lists and returns a single set with the values that each list has in common
+def get_values_in_common(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    if len(set1.intersection(set2)) > 0:
+        return (set1.intersection(set2))
 
 assert get_values_in_common([5, 1, 2, 3], [3, 4, 5, 5]) == {3, 5}
 assert get_values_in_common([1, 2], [2, 2, 3]) == {2}
@@ -1484,6 +1493,11 @@ print("Exercise 85 is correct.")
 
 # Exercise 86
 # Write a function definition named get_values_not_in_common that takes two lists and returns a single set with the values that each list does not have in common
+def get_values_not_in_common(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    if len(set1.intersection(set2)) == 0:
+        return (set1.intersection(set2))
 
 assert get_values_not_in_common([5, 1, 2, 3], [3, 4, 5, 5]) == {1, 2, 4}
 assert get_values_not_in_common([1, 1], [2, 2, 3]) == {1, 2, 3}
